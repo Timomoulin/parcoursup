0dcd2f1679ef61d6c6254a52b16bf027a175c9e5
T.Moulin <timomoulin@msn.com>
2024-01-19 08:41:27 +0100

    avant demo

--- src/main/resources/import2.sql
+++ src/main/resources/import2.sql
@@ -2,7 +2,7 @@ insert into role (nom) values ('admin');
 insert into role (nom) values ('etudiant');
 insert into utilisateur ( email, mdp,est_victime) values ('admin@email.com','$2a$10$MJn5SDUCgsm3XEyvELGQI.lcCzCXtxhA8hunr9jX9yvDd6/FkjxYO',false);
 -- mdp = pacourSup2023=super
-insert into utilisateur (est_victime, mdp, nom, num_dossier, prenom) values (false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','vigna','050489','edith'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','milesi','15892','audrey'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','hourdeaux','58924','laurence'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','sequier','81547','catherine'),(false,'$2y$10$Hl/YRWK8.WsgLaoFPqSfB.JPHcRSsEzK1U3GCeC.lmUyS/kmDlTQS','bendib','84651','bahija'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','jules','87241','laurent'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','guillien','62456','yann'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','moulin','62456','timothée');
+insert into utilisateur (est_victime, mdp, nom, num_dossier, prenom) values (false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','vigna','050489','edith'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','milesi','15892','audrey'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','hourdeaux','58924','laurence'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','sequier','81547','catherine'),(false,'$2y$10$Hl/YRWK8.WsgLaoFPqSfB.JPHcRSsEzK1U3GCeC.lmUyS/kmDlTQS','bendib','84651','bahija'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','jules','87241','laurent'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','guillien','62456','yann'),(false,'$2y$10$DZsKQfsp4gfnmG2z.FTLRejYSaoZlBt7wOoEeuafqcHa4yAItc1c2','moulin','62906','timothée');
 -- mdp = motdepassehypersecret2023!
 insert into utilisateur (est_victime, code_secret, mdp, nom, num_dossier, prenom) values (true,'rjcpwh','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste1','57721','poste1'),(true,'ygrgjk','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste2','22754','poste2'),(true,'viking','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste3','97324','poste3'),(true,'daredevil','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste4','43013','poste4');
 insert into utilisateur (est_victime, code_secret, mdp, nom, num_dossier, prenom) values (true,'jftmogh','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste5','59026','poste5'),(true,'ytrfyjx','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste6','72054','poste6'),(true,'asgard','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste7','17320','poste7'),(true,'murdock','$2y$10$7oyvjE1esYgl.062Mph8cepT9/hFq5EY2tf5hxBt3b/KZIP2xCITG','poste8','66015','poste8');
@@ -19,15 +19,43 @@ insert into voeu (num_ordre, formation_id, utilisateur_id) VALUES (1,23129,10),(
 insert into voeu (num_ordre, formation_id, utilisateur_id) VALUES (1,23129,14),(1,23130,15),(1,23128,16),(1,23128,17);
 insert into  voeu (num_ordre, formation_id, utilisateur_id) values (1,20379,1),(1,20379,2),(1,20379,3),(1,20379,4),(1,20379,5),(1,20379,6),(1,20379,7),(1,20379,8),(1,20379,9);
 
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 2, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 2, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 2, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 3, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 3, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 3, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 4, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 4, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 4, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 5, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 5, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 5, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 6, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 6, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 6, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+INSERT INTO `log` ( `log_date_time`, `utilisateur_id`, `action`, `detail`, `ip`) VALUES
+( '2024-01-01 07:00:22.715085', 6, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:04:26.718354', 6, 'Authentification réussie', '', '192.168.1.1'),
+( '2024-01-14 07:06:42.610051', 6, 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.168.1.1');
+
+
+
+
 INSERT INTO log ( `action`, `detail`, `ip`, `log_date_time`, `utilisateur_id`) VALUES
-                                                                                          ( 'Authentification réussie', '', '192.167.7.89', '2024-01-14 16:30:30.456351', 10),
-                                                                                          ( 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.167.7.89', '2024-01-14 16:35:52.467318', 10),
-                                                                                          ( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:38.083412', 10),
-                                                                                          ( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:43.337181', 10),
-                                                                                          ('Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:48.847701', 10),
-                                                                                          ( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:58.976401', 10),
-                                                                                          ( 'Authentification réussie', '', '172.17.200.2', '2024-01-17 23:03:03.206237', 10),
-                                                                                          ( 'Changement du voeu n°1', 'Nouvelle formation = BTS Gestion des Petits Enfants, Nouveau lieu = Lycée Saint-Pierre (Saint-Pierre)', '172.17.200.2', '2024-01-17 23:03:31.157276', 10);
+( 'Authentification réussie', '', '192.167.7.89', '2024-01-14 16:30:30.456351', 10),
+( 'Ajout d\'un nouveau voeu (voeu n°1', 'Formation = BTS SIO - Services informatiques aux organisations, Lieu = Lycée Leonard De Vinci (Melun - 77) (Melun)', '192.167.7.89', '2024-01-14 16:35:52.467318', 10),
+( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:38.083412', 10),
+( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:43.337181', 10),
+('Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:48.847701', 10),
+( 'Authentification échec', '', '172.17.200.2', '2024-01-17 23:02:58.976401', 10),
+( 'Authentification réussie', '', '172.17.200.2', '2024-01-17 23:03:03.206237', 10),
+( 'Changement du voeu n°1', 'Nouvelle formation = BTS Gestion des Petits Enfants, Nouveau lieu = Lycée Saint-Pierre (Saint-Pierre)', '172.17.200.2', '2024-01-17 23:03:31.157276', 10);
 
 INSERT INTO log ( `action`, `detail`, `ip`, `log_date_time`, `utilisateur_id`) VALUES
                                                                                    ( 'Authentification réussie', '', '192.167.7.89', '2024-01-14 16:30:30.456351', 11),
